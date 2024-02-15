2f7323c15b2204df79e38e45a03b2c9095d8b5b4
Sasas <zipppycole@gmail.com>
2024-02-15 13:43:37 +0800

    making changes in the button fucntion and make simple calculator in the html

--- HTML/function.js
+++ HTML/function.js
@@ -1,1 +1,13 @@
-function myFunction() { document.getElementById("xx").innerHTML += "1" };
+function comment()
+{
+    let a = document.getElementById("xx"), b = document.getElementById("firstNumber").value, c = document.getElementById("secondNumber").value;
+
+    // Check if the first and second is a number
+    if(isNaN(b) && isNaN(c)) return 0;
+    var val1 = parseInt(b);
+    var val2 = parseInt(c);
+
+    let sum = val1+val2;
+
+    alert("sum result: "+sum);
+}

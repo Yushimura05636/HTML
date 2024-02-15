caed64aab44dc2f4a4b6b2b725eeefa1a347cbe8
Sasas <zipppycole@gmail.com>
2024-02-15 12:31:10 +0800

    Making changes to the main() and add two methods() such as Message(string); showMessage();

--- Java_Test/java_test.java
+++ Java_Test/java_test.java
@@ -1,6 +1,13 @@
 package Java_Test;
 public class java_test {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        showMessage();
+    }
+    private static void Message(String message) throws Exception {
+        System.out.println(message);
+    }
+    private static void showMessage()
+    {
+        System.out.println("This is the fucntion for showing numbers.");
     }
 }
